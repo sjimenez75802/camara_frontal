054ca2a863b0cce9127cdf6c55569d4d08cea38c
Santiago Jimenez Marin <sjimenez75802@gmail>
2021-03-23 08:08:22 -0500

    third edition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
+//constantes
 const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+let broadcaster;
 //creacion de un puerto
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+
 //
 const router = require('./router');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
