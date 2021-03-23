13e69c0fce7d39f1a100460a5398a5b66505480c
Santiago Jimenez Marin <sjimenez75802@gmail>
2021-03-23 08:22:20 -0500

    five edition

--- src/router.js
+++ src/router.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index', { title: 'First Web Node' });
   });
-
+  router.get('/ver', (req, res) => {
+    res.render('ver', { title: 'First Web Node' });
+  });
   module.exports = router;
